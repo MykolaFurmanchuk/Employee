e2466a502d30977c5141b7583ae8247a89df24b2
mike2205-5 <57373412+mike2205-5@users.noreply.github.com>
2021-05-11 20:51:08 +0300

    Added Entities to DAL

---
+++ Employee/DAL/Entities/Employeers.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace DAL.Entities
+{
+    public class Employeers
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public ICollection<Posady> Posady { get; set; }
+        public TrudBook TrudBooks { get; set; }
+    }
+}
---
+++ Employee/DAL/Entities/TrudBook.cs
@@ -0,0 +1,9 @@
+﻿namespace DAL.Entities
+{
+    public class TrudBook
+    {
+        public int TrudBookID { get; set; }
+        public double Expierence { get; set; }
+        public Employeers employeers { get; set; }
+    }
+}
