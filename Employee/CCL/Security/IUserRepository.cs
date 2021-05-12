15480b68f2f2a0cb3f0c65d48e2c45f32442f1d3
mike2205-5 <57373412+mike2205-5@users.noreply.github.com>
2021-05-12 10:54:54 +0300

    Added All CCl

---
+++ Employee/CCL/Security/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using DAL.Entities;
+using DAL.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using DAL.EF;
+using CCL.Security.Identity;
+
+namespace Catalog.DAL.Repositories.Interfaces
+{
+    public interface IUserRepository
+        : IRepository<User>
+    {
+    }
+}
---
+++ Employee/CCL/Security/UserRepository.cs
@@ -0,0 +1,18 @@
+﻿using DAL.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using DAL.EF;
+using CCL.Security.Identity;
+
+namespace DAL.Repositories.Impl
+{
+    public class UserRepository
+         : BaseRepository<User>
+    {
+        public UserRepository(EmployeersContext context) : base(context)
+        {
+        }
+    }
+}
