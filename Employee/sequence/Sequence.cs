773d62f841322b0f7aead91549f289e8e16d7c98
mike2205-5 <57373412+mike2205-5@users.noreply.github.com>
2021-05-15 14:25:03 +0300

    Sequence

---
+++ Employee/sequence/Sequence.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace sequence
+{
+    public class Sequence
+    {
+        static void Main(string[] args)
+        {
+            Archive archive = new Archive();
+            Reader reader = new Reader();
+            reader.SeeRecords(archive);
+            Console.Read();
+        }
+    }
+    class Reader
+    {
+        public void SeeRecords(Archive archive)
+        {
+            IRecordIterator iterator = archive.CreateNumerator();
+            while (iterator.HasNext())
+            {
+                Record record= iterator.Next();
+                Console.WriteLine(record.Date);
+            }
+        }
+    }
+    interface IRecordIterator
+    {
+        bool HasNext();
+        Record Next();
+    }
+    interface IRecordNumerable
+    {
+        IRecordIterator CreateNumerator();
+        int Count { get; }
+        Record this[int index] { get; }
+    }
+    class Record
+    {
+        public string Date { get; set; }
+    }
+
+    class Archive : IRecordNumerable
+    {
+        private Record[] records;
+        public Archive()
+        {
+            records = new Record[]
+            {
+            new Record {Date="2021-04-12"},
+            new Record {Date="2021-03-12"},
+            new Record {Date="2021-02-12"},
+            new Record {Date="2021-01-12"},
+            new Record {Date="2021-05-12"}
+            };
+        }
+        public int Count
+        {
+            get { return records.Length; }
+        }
+
+        public Record this[int index]
+        {
+            get { return records[index]; }
+        }
+        public IRecordIterator CreateNumerator()
+        {
+            return new RecordNumerator(this);
+        }
+    }
+    class RecordNumerator : IRecordIterator
+    {
+        IRecordNumerable aggregate;
+        int index = 0;
+        public RecordNumerator(IRecordNumerable a)
+        {
+            aggregate = a;
+        }
+        public bool HasNext()
+        {
+            return index < aggregate.Count;
+        }
+
+        public Record Next()
+        {
+            return aggregate[index++];
+        }
+    }
+}
